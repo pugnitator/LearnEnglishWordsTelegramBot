f00c7fa1146e4ba8df4fb5f1ceb1b977d913a000
pugnitator <pugnitator2000@gmail.com>
2024-01-09 10:56:14 +0300

    Add words file

---
+++ src/main/kotlin/dictionary/Files.kt
@@ -0,0 +1,14 @@
+package org.example.dictionary
+
+import java.io.File
+
+fun main() {
+    val wordsFile = File("words.txt")
+//    wordsFile.createNewFile()
+
+//    wordsFile.appendText("hello привет")
+//    wordsFile.appendText("dog собака")
+//    wordsFile.appendText("cat кошка")
+
+    println(wordsFile.readLines())
+}
